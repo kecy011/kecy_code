494134c7a253c69093bc7b251ba0c818f31c19fb
kecy011 <kelechiaharanwa011@gmail.com>
2023-03-04 13:02:10 +0100

    third commit 04/03/23

---
+++ PycharmProjects/pythonProject/pythonProject1 student class/Impulse.py
@@ -0,0 +1,21 @@
+class Impulse:
+
+    def __init__(self, force,time, mass, velocity):
+        self.force = force
+        self.time = time
+        self.mass = mass
+        self.velocity = velocity
+
+    def get_force(self, mass, acceleration): 
+        return mass * acceleration 
+
+    def get_time(self, velocity, initial_velocity, acceleration):
+        pass
+
+    def get_mass(force, acceleration):
+        return force/acceleration
+
+    def get_velocity(self, u, a, t):
+        return u + (a*t)
+                               
+
---
+++ PycharmProjects/pythonProject/pythonProject1 student class/main.py
@@ -0,0 +1,50 @@
+
+from Student import Student
+student1 = Student("kelechi", "medical lab tech", 3.46,False)
+student2 = Student("oyewunmi oluwatobiloba dorcas", "med lab tech", 3.1, False)
+
+
+print(student1.is_on_probation)
+print(student2.name)
+print(type(Student))
+name = "kelechi"
+
+while len(name) < 5:
+    print("the length of your name is", len(name))
+    len(name)==(float(len(name)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
